ff07cd77a3c2699dbcef53439fd03c31b585c108
John Estabrook <jestabro@vyos.io>
2024-01-30 10:08:36 -0600

    add load_config

--- src/vyos1x_adapter.mli
+++ src/vyos1x_adapter.mli
@@ -1,8 +1,12 @@
+open Vyos1x
+
 val handle_init : unit -> int
 val handle_free : int -> unit
 val in_config_session_handle : int -> bool
 val in_config_session : unit -> bool
 val set_path : int -> string list -> int -> string
 val delete_path : int -> string list -> int -> string
+val set_path_reversed : int -> string list -> int -> string
+val delete_path_reversed : int -> string list -> int -> string
 
-val test_if_session : unit -> string
+val load_config : Config_tree.t -> Config_tree.t -> string

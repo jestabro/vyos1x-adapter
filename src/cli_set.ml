0a90ec1810604c348b9c4f0daa21926ec5dd2215
John Estabrook <jestabro@vyos.io>
2024-10-15 23:30:27 -0500

    retest integration

--- src/cli_set.ml
+++ src/cli_set.ml
@@ -12,6 +12,7 @@ let print_res res =
         Printf.printf "no error\n"
 
 let legacy = ref false
+let no_set = ref false
 (*let path_arg = ref []
 let args = []
 let usage = Printf.sprintf "Usage: %s <path>" Sys.argv.(0)
@@ -27,37 +28,60 @@ let usage = "Usage: " ^ Sys.argv.(0) ^ " [options]"
 
 let args = [
     ("--path", Arg.String (fun s -> path_opt := s), "<string> Configuration path");
+    ("--legacy", Arg.String (fun _ -> legacy := true), "<bool> Use legacy validation");
+    ("--no-set", Arg.String (fun _ -> no_set := true), "<bool> Do not set path");
    ]
+
+let get_sockname =
+    "/var/run/vyconfd.sock"
+
+let run socket path_list =
+    let%lwt token = VC.session_init socket in
+    match token with
+    | Error e -> Error e |> Lwt.return
+    | Ok token ->
+        let%lwt out = VC.session_validate_path socket token path_list
+        in
+        let%lwt _ = VC.session_free socket token in
+        Lwt.return out
+(*        match out with
+        | Error e -> Lwt.return ("Failed to validate path: " ^ e)
+        | Ok _ -> Lwt.return ("Appeared to work")
+*)
+let validate_path path =
+    let socket = get_sockname in
+    let path_list = Vyos1x.Util.list_of_path path in
+    try
+        let out = Lwt_main.run (run socket path_list) in
+        print_endline (Result.value out ~default:""); 0
+    with exn e -> print_endline ("Error: " ^ e); 1
 
 
 let () =
     let () = Arg.parse args (fun _ -> ()) usage in
 (*    let path_set = List.rev !path_arg in *)
 (*    let path_set = Vyos1x.Util.list_of_path !path_opt in *)
-    if not !legacy then
-        let token = VC.session_init () in
-        print_endline token;
-        let out = VC.session_status token in
-        print_endline out
-(*        let res_valid = VC.session_validate_path token path_set in *)
-(*        let res_valid = VC.session_show_config token path_set in
-        Printf.printf "%s\n" res_valid;
-    *)
-(*        ignore(VC.session_free token); *)
-(*
-    let h = Vyos1x_adapter.cstore_handle_init () in
-    if not (Vyos1x_adapter.cstore_in_config_session_handle h) then
-        (Vyos1x_adapter.cstore_handle_free h;
-        Printf.printf "not in config session\n")
-    else
-        let res_valid =
-        if !legacy then
-            Vyos1x_adapter.legacy_validate_path h path_set
-        else ""
-        in
-        let res_set = Vyos1x_adapter.cstore_set_path h path_set in
-        Printf.printf "\nSetting [%s]\n" (String.concat " " (path_set));
-        print_res (res_valid ^ "\n" ^ res_set);
-        Vyos1x_adapter.cstore_handle_free h
-*)
-
+    let res =
+        if not !legacy then validate_path !path_opt
+        else 0
+    in
+    if !legacy || not !no_set then
+        let h = Vyos1x_adapter.cstore_handle_init () in
+        if not (Vyos1x_adapter.cstore_in_config_session_handle h) then
+            (Vyos1x_adapter.cstore_handle_free h;
+            Printf.printf "not in config session\n");
+            exit 1
+        else
+            let res_valid =
+            if !legacy then
+                Vyos1x_adapter.legacy_validate_path h path_set
+            else ""
+            in
+            let res_set =
+                if not !no_set then
+                    Vyos1x_adapter.cstore_set_path h path_set
+                else ""
+            in
+            Printf.printf "\nSetting [%s]\n" (String.concat " " (path_set));
+            print_res (res_valid ^ "\n" ^ res_set);
+            Vyos1x_adapter.cstore_handle_free h
